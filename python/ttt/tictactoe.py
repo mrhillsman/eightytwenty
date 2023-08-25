96fc98809465a51c5d6bd8a3acd555b5066ac3b3
Melvin Hillsman <mhillsma@redhat.com>
2023-08-25 00:44:36 -0500

    Mockup, pipenv, and initial python app
    Signed-off-by: Melvin Hillsman <mhillsma@redhat.com>

--- python/ttt/tictactoe.py
+++ python/ttt/tictactoe.py
@@ -1,67 +1,71 @@
 from tkinter import *
+import random
+
+# Global variables
+current_player = ""
+symbol = ""
+game_over = False
+winner = ""
+tie = False
+move_count = 0
+game_started = False
 
 
-def create_grid(event=None):
-    w = c.winfo_width() # Get current width of canvas
-    h = c.winfo_height() # Get current height of canvas
-    c.delete('grid_line') # Will only remove the grid_line
+def initialize_game():
+    current_player = ""
+    symbol = ""
+    game_over = False
+    winner = ""
+    tie = False
+    move_count = 0
+    game_started = False
 
-    # Creates all vertical lines at intevals of 100
-    for i in range(0, w, 120):
-        c.create_line([(i, 0), (i, h)], tag='grid_line')
+    reset_gameboard()
 
-    # Creates all horizontal lines at intevals of 100
-    for i in range(0, h, 120):
-        c.create_line([(0, i), (w, i)], tag='grid_line')
+    random.Random(random.seed(float)).random()
 
 
-root = Tk()
 
-c = Canvas(root, height=960, width=960, bg='white')
-c.pack(fill=BOTH, expand=True)
 
-c.bind('<Configure>', create_grid)
+def reset_gameboard():
+    pass
 
-root.mainloop()
 
-'''
-from tkinter import *
-from tkinter import ttk
+def switch_player():
+    pass
 
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-    except ValueError:
+
+def place_symbol():
+    pass
+
+
+def start_game():
+    pass
+
+
+def reset_game():
+    pass
+
+
+def update_gameboard():
+    pass
+
+
+def check_for_winner():
+    def check_rows():
         pass
 
+    def check_columns():
+        pass
+
+    def check_diagonals():
+        pass
+
+    check_rows()
+    check_columns()
+    check_diagonals()
+
+
 root = Tk()
-root.title("Feet to Meters")
-
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
-
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
-
-feet_entry.focus()
-root.bind("<Return>", calculate)
 
 root.mainloop()
-'''
-
